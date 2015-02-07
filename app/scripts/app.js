0e51ffc356a6ae6ad67b9b41530b3f6e2bfa7ef3
Shashvat Tripathi <shashvattrip@gmail.com>
2015-02-07 21:00:45 +0530

    Setting up Yeoman boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name taskrApp
+ * @description
+ * # taskrApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('taskrApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
